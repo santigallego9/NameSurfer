3eb1b886d531e3da0a2eb2d06cb933f3ef25070f
Santi Gallego <santigallego9@gmail.com>
2016-07-23 19:00:22 -0500

    app

--- app/src/main/java/cs193a/stanford/edu/namesurfer/NameSurferActivity.java
+++ app/src/main/java/cs193a/stanford/edu/namesurfer/NameSurferActivity.java
@@ -16,6 +16,9 @@ import android.widget.EditText;
 import android.widget.Switch;
 import com.jjoe64.graphview.*;
 import com.jjoe64.graphview.series.*;
+
+import java.util.Scanner;
+
 import stanford.androidlib.*;
 import stanford.androidlib.data.*;
 
@@ -32,6 +35,26 @@ public class NameSurferActivity extends SimpleActivity {
         handleEnterKeyPress(findEditText(R.id.name));
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinX(START_YEAR);
+        graph.getViewport().setMaxX(END_YEAR);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(MAX_RANK);
+
+        // read example.sql into database named "example"
+        /*SQLiteDatabase db = openOrCreateDatabase("babynames");
+        Scanner scan = new Scanner(getResources()
+                .openRawResource(R.raw.babynames));
+
+        String query = "";
+        while (scan.hasNextLine()) { // build and execute queries
+            query += scan.nextLine() + "\n";
+            if (query.trim().endsWith(";")) {
+                db.execSQL(query);
+                query = "";
+            }
+        }*/
     }
 
     // Called when the user presses Enter on the text box. Runs a query.
@@ -48,9 +71,42 @@ public class NameSurferActivity extends SimpleActivity {
     // This method performs a query on the babynames database
     // for the given name/sex and prints the results.
     private void doQuery() {
+        GraphView graph = (GraphView) findViewById(R.id.graph);
         EditText edit = (EditText) findViewById(R.id.name);
+        String name = edit.getText().toString();
         Switch sexSwitch = (Switch) findViewById(R.id.sex);
+        String sex = sexSwitch.isChecked() ? "F" : "M";
 
         // TODO
+        /* Database name = "babynames"
+         * Tables:
+         *   ranks: name,   sex, year, rank
+         *         "Martin, "M", 1880, 145
+         */
+
+        String query =  "SELECT * " +
+                        "FROM ranks " +
+                        "WHERE name = \"" + name + "\" " +
+                        "AND sex = \"" + sex + "\"";
+
+        SQLiteDatabase db = openOrCreateDatabase("babynames");
+        Cursor cr = db.rawQuery(query, null);
+
+        LineGraphSeries<DataPoint> line = new LineGraphSeries<>();
+
+        try {
+            for (SimpleRow row : SimpleDatabase.rows(cr)) {
+                int year = row.get("name");
+                int rank = row.get("rank");
+
+                DataPoint point = new DataPoint(year, rank);
+                line.appendData(point, false, 13);
+            }
+        } catch (Exception e) {
+            toast("There was an error in your query");
+        }
+
+        graph.addSeries(line);
+
     }
 }
